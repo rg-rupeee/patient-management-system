5adfcfa21924a825fbd6ebab5202dca7a9972bbf
rg-rupeee <rupeshgarhwal3920@gmail.com>
2025-05-25 15:35:44 +0530

    Delete Patient

--- patient-service/src/main/java/com/rupesh/patient_service/service/PatientService.java
+++ patient-service/src/main/java/com/rupesh/patient_service/service/PatientService.java
@@ -59,4 +59,12 @@ public class PatientService {
 
 		return PatientMapper.toDTO(updatedPatient);
 	}
+
+	public void deletePatient(UUID id){
+		Patient patient = patientRepository.findById(id).orElseThrow(
+				() -> new PatientNotFoundException("Patient not found with ID: " + id)
+		);
+
+		patientRepository.deleteById(id);
+	}
 }
