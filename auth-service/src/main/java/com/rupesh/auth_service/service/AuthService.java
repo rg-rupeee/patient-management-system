9413c4f96529e7c944489d6cb68491bc782df2c3
rg-rupeee <rupeshgarhwal3920@gmail.com>
2025-08-17 12:26:38 +0530

    authservice validate token

--- auth-service/src/main/java/com/rupesh/auth_service/service/AuthService.java
+++ auth-service/src/main/java/com/rupesh/auth_service/service/AuthService.java
@@ -3,6 +3,7 @@ package com.rupesh.auth_service.service;
 import com.rupesh.auth_service.dto.LoginRequestDTO;
 import com.rupesh.auth_service.model.User;
 import com.rupesh.auth_service.util.JwtUtil;
+import io.jsonwebtoken.JwtException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -25,5 +26,14 @@ public class AuthService {
 				.findByEmail(loginRequestDTO.getEmail())
 				.filter(u -> passwordEncoder.matches(loginRequestDTO.getPassword(), u.getPassword()))
 				.map(u -> jwtUtil.generateToken(u.getEmail(), u.getRole()));
+	}
+
+	public boolean validateToken(String token){
+		try{
+			jwtUtil.validateToken(token);
+			return true;
+		} catch (JwtException e){
+			return false;
+		}
 	}
 }
